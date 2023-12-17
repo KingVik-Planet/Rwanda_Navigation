e5941cf60f3d69e5b5a80d486575f3d7e4f6ca67
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-17 17:37:49 +0100

    Update

--- pages/Maps.py
+++ pages/Maps.py
@@ -1,7 +1,21 @@
 import streamlit as st
-from streamlit_folium import folium_static
 import folium
-from folium import plugins
+from streamlit_folium import folium_static
+import geocoder
+
+# Function to perform the search
+def search_location(query):
+    location = geocoder.osm(query)
+    if location.ok:
+        m = folium.Map(location=[location.latlng[0], location.latlng[1]], zoom_start=14)
+        folium.Marker(
+            location=[location.latlng[0], location.latlng[1]],
+            popup=query,
+            icon=folium.Icon(color='blue')
+        ).add_to(m)
+        return m
+    else:
+        return None
 
 # Create a Streamlit app
 st.title("Interactive Map with Streamlit")
@@ -10,39 +24,21 @@ st.title("Interactive Map with Streamlit")
 kigali_location = [-1.9536, 30.0606]
 m = folium.Map(location=kigali_location, zoom_start=12)
 
-# Add OpenStreetMap tiles as the background
-folium.TileLayer('openstreetmap').add_to(m)
-
-# Add Stamen Terrain tiles as a satellite view
-folium.TileLayer('Stamen Terrain').add_to(m)
-
-# Add Stamen Toner tiles
-folium.TileLayer('Stamen Toner').add_to(m)
-
-# Add Stamen Water Color tiles
-folium.TileLayer('Stamen Water Color').add_to(m)
-
-# Add CartoDB Positron tiles
-folium.TileLayer('CartoDB Positron').add_to(m)
-
-# Create a FeatureGroup to hold base maps
-base_maps = folium.FeatureGroup(name='Base Maps').add_to(m)
-
-# Add the base maps to the FeatureGroup
-folium.TileLayer('openstreetmap').add_to(base_maps)
-folium.TileLayer('Stamen Terrain', attr='Stamen').add_to(base_maps)
-folium.TileLayer('Stamen Satellite', attr='Stamen').add_to(base_maps)
-folium.TileLayer('Stamen Toner').add_to(base_maps)
-folium.TileLayer('Stamen Water Color').add_to(base_maps)
-folium.TileLayer('CartoDB Positron').add_to(base_maps)
-
-# Add layer control for switching between base maps
-folium.LayerControl().add_to(m)
-
-# Add a search box
-search = plugins.Search(layer=base_maps, geom_type='Point')  # Pass the FeatureGroup to the layer parameter
-search.add_to(m)
-
 # Display the map using folium_static
 st.write("Interactive Map:")
 folium_static(m)
+
+# User input for location
+query = st.text_input("Enter Location of Interest:")
+
+# Check if the query is not empty
+if query:
+    # Search for the location
+    result_map = search_location(query)
+
+    # Display the result map using folium_static
+    if result_map:
+        st.write("Map for:", query)
+        folium_static(result_map)
+    else:
+        st.warning("Location not found. Please enter a valid location.")
